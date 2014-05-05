c6f7ea1fc143af032d2ab3714480e0bf3abb289f
Tim Golden <mail@timgolden.me.uk>
2014-05-05 15:23:27 +0100

    Add sample make.cmd

---
+++ make.cmd
@@ -0,0 +1,19 @@
+@echo off
+REM
+REM This is intended to be a convenience file when the layout
+REM is something like this:
+REM
+REM python\
+REM   make.cmd (this file)
+REM   python-make\
+REM     make.py
+REM     configure.ini
+REM   issue12345\
+REM     (a python clone)
+REM   issue56789\
+REM     (a different python clone)
+REM
+REM You would normally be inside one of the Python clones and then calling
+REM ..\make
+REM
+%~d0%~p0\python-make\make.py %*
